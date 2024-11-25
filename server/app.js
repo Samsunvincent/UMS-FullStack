5f713798b044e5dcaee3af1c7f01d8775ec04eb0
Samsunvincent <samsunvincent143@gmail.com>
2024-11-25 11:14:03 +0530

    ums full stack completed

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const mongoConnect = require('../server/db/connect');
+const mongoConnect = require('./db/connect');
 mongoConnect();
 const cors  = require('cors')
 const path = require('path')
@@ -10,12 +10,12 @@ const https = require('https');
 const fs = require('fs');
 
 const router = require('./Router/router')
-const authRouter = require('../server/Router/auth-Router')
+const authRouter = require('./Router/auth-Router')
 
 app.use(express.json({limit : "500mb"}));
 app.use(express.urlencoded({extended : true}));
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'client')));
+// app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(router)
 app.use(authRouter)
